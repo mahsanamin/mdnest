4e68c4642d1f4862eed300edfce9492058227e7e
Ahsan Amin <mhsanamin@gmail.com>
2026-08-02 16:34:10 +0500

    chore: open develop at 4.1.1-dev

--- backend/handlers/config.go
+++ backend/handlers/config.go
@@ -114,7 +114,7 @@ func (h *ConfigHandler) HandleConfig(w http.ResponseWriter, r *http.Request) {
 		"liveCollab":   h.liveCollab,
 		"require2FA":   h.require2FA,
 		"userProvider": h.userProvider,
-		"version":      "4.1.0",
+		"version":      "4.1.1-dev",
 		"commit":       Commit,
 		"buildTime":    BuildTime,
 	}
